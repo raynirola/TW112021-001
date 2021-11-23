62c8d04e2f950e151c1f29aff81d4c450b8dc08a
Ray Nirola <hi@raynirola.com>
2021-11-23 13:58:14 +0530

    Fresh NextJS application

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import { Fragment } from "react";
+
+const Home = () => (
+  <Fragment>
+    <Head>
+      <title>Webflow Clone</title>
+      <meta name="description" content="1:1 Clone of a Webflow site."/>
+    </Head>
+
+    <main>
+      <h1>Work In Progress</h1>
+    </main>
+  </Fragment>
+);
+
+
+export default Home

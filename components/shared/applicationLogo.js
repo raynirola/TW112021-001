b8736f2db3aa87b18a93f497d286e767674a7e5c
Ray Nirola <hi@raynirola.com>
2021-11-24 14:12:25 +0530

    Fixed positioning, its placed absolute now.

--- components/shared/applicationLogo.js
+++ components/shared/applicationLogo.js
@@ -1,11 +1,12 @@
 const ApplicationLogo = () => {
   return (
-    // eslint-disable-next-line @next/next/no-html-link-for-pages
-    <a href="/" className="absolute top-[35px] left-[42px]">
-      {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img src="https://assets.website-files.com/5f5f94343affd51e900cd45c/5f5f94343affd54edd0cd4af_byra-logo.svg"
-           loading="lazy" width="75" height="" id="top" alt=""/>
-    </a>
+    <header className="absolute top-0 bg-white bg-opacity-[0.05] md:bg-transparent w-full h-[100px]">
+      {/* eslint-disable-next-line @next/next/no-html-link-for-pages */ }
+      <a href="/" className="absolute top-[35px] left-[42px]">
+        <img src="https://assets.website-files.com/5f5f94343affd51e900cd45c/5f5f94343affd54edd0cd4af_byra-logo.svg"
+             loading="lazy" width="75" height="" id="top" alt=""/>
+      </a>
+    </header>
   )
 }
 

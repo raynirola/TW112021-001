0c4f31cb6aa8863bb817c962bb847458563829b9
Ray Nirola <hi@raynirola.com>
2021-11-24 15:58:11 +0530

    Animation WIP

--- components/header/index.js
+++ components/header/index.js
@@ -1,5 +1,6 @@
 import { ApplicationLogo } from "../shared/applicationLogo";
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
+import { Transition } from "@headlessui/react";
 
 const slides = [
   {
@@ -32,23 +33,55 @@ const Header = () => {
           const nextSlide = slides[(currentSlide.id + 1) % slides.length];
           setCurrentSlide(nextSlide);
         },
-        2000
+        3000
       );
       return () => clearInterval(interval);
     },
     [ currentSlide ]
   )
 
-  const SingleSlide = ({ slide }) => <div className="relative min-h-full inset-0 flex items-center">
-    <div className="absolute inset-0 opacity-80">
-      {/* eslint-disable-next-line @next/next/no-img-element */ }
-      <img src={ slide.image } alt={ slide.subtitle } loading="lazy" className="w-full h-full object-center object-cover"/>
-    </div>
-    <div className="w-full text-left text-white leading-none font-BebasNeue relative max-w-5xl mx-auto">
-      <p className="tracking-[6px]">{ slide.subtitle }</p>
-      <p className="text-[160px] uppercase tracking-[5px] mt-4">{ slide.title }</p>
-    </div>
-  </div>;
+  const SingleSlide = ({ slide }) => {
+    return (
+      <Transition
+        appear
+        show={ true }
+        as="div"
+        enter="transition-opacity duration-1000"
+        enterFrom="opacity-0"
+        enterTo="opacity-80"
+        className="relative min-h-full inset-0 flex items-center"
+      >
+        <>
+          <Transition.Child
+            as={Fragment}
+            enter="transition ease-in-out duration-300 transform"
+            enterFrom="translate-x-full"
+            enterTo="translate-x-0 opacity-80"
+            leave="transition ease-in-out duration-300 transform"
+            leaveFrom="translate-x-0"
+            leaveTo="translate-x-full"
+          >
+            {/* eslint-disable-next-line @next/next/no-img-element */ }
+            <img src={ slide.image } alt={ slide.subtitle } loading="lazy"
+                 className="absolute inset-0 w-full h-full object-center object-cover"/>
+          </Transition.Child>
+
+          <Transition.Child
+            beforeEnter={() => setTimeout(() => {}, 1000)}
+            as={Fragment}
+            enter="transition ease-in-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+          >
+          <div className="w-full text-left text-white leading-none font-BebasNeue relative max-w-5xl mx-auto">
+            <p className="tracking-[6px]">{ slide.subtitle }</p>
+            <p className="text-[160px] uppercase tracking-[5px] mt-4">{ slide.title }</p>
+          </div>
+          </Transition.Child>
+        </>
+      </Transition>
+    )
+  }
 
   return (
     <section className="h-screen relative bg-black overflow-x-hidden">
